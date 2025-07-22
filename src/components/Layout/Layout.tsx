7e2896039b26848deb06d218fef1b75d5b2e8ec5
lucasbeneti <lbeneti@gmail.com>
2025-07-22 10:56:19 -0300

    adding accordion component to the app, which will be useful for the about page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { AppSidebar } from '../app-sidebar/app-sidebar';
 
 export function LayoutWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider className='dark' defaultOpen={false}>
+    <SidebarProvider className='dark' defaultOpen>
       <AppSidebar />
       <main className='dark:bg-gray-900 w-full'>
         <SidebarTrigger
