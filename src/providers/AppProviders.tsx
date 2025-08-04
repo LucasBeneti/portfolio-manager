806082d7f929bfca863bdcf17dc976e5ee1b1717
lucasbeneti <lbeneti@gmail.com>
2025-08-04 14:44:54 -0300

    build fix

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -3,7 +3,6 @@ import { LayoutWrapper } from '@/components/Layout';
 import { UserInformationProvider } from '@/context/user-information';
 import { DialogProvider } from '@/context/dialog';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
