baa02765b020f09a1a39cd705f8db70cd3abc67d
lucasbeneti <lbeneti@gmail.com>
2025-09-18 14:14:46 -0300

    implemented the fixes for the chart data and colors

--- src/components/portfolio-allocation-chart/PortfolioAllocationChart.tsx
+++ src/components/portfolio-allocation-chart/PortfolioAllocationChart.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import * as React from 'react';
-import { TrendingUp } from 'lucide-react';
+import { LucideArrowBigUp, LucideArrowBigDown } from 'lucide-react';
 import { Label, Pie, PieChart } from 'recharts';
 
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -57,14 +56,15 @@ export function PortfolioAllocationChart() {
   const compAssets = computedUserAssets(assets);
   const formattedData = convertComputedAssetsToChart(compAssets);
 
-  const totalVisitors = React.useMemo(() => {
+  const totalInvested = React.useMemo(() => {
     return formattedData.reduce((acc, curr) => acc + curr.amountInvested, 0);
   }, [formattedData]);
   return (
-    <Card className='flex flex-col'>
+    <Card className='flex flex-col bg-gray-950'>
       <CardHeader className='items-center pb-0'>
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle className='text-2xl text-center'>
+          Total investido da carteira
+        </CardTitle>
       </CardHeader>
       <CardContent className='flex-1 pb-0'>
         <ChartContainer
@@ -80,7 +80,7 @@ export function PortfolioAllocationChart() {
               data={formattedData}
               dataKey='amountInvested'
               nameKey='investment'
-              innerRadius={60}
+              innerRadius={75}
               strokeWidth={5}
             >
               <Label
@@ -96,16 +96,19 @@ export function PortfolioAllocationChart() {
                         <tspan
                           x={viewBox.cx}
                           y={viewBox.cy}
-                          className='fill-foreground text-3xl font-bold'
+                          className='fill-foreground text-lg font-bold'
                         >
-                          {totalVisitors.toLocaleString()}
+                          {new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
+                          }).format(totalInvested)}
                         </tspan>
                         <tspan
                           x={viewBox.cx}
                           y={(viewBox.cy || 0) + 24}
                           className='fill-muted-foreground'
                         >
-                          Visitors
+                          Investidos
                         </tspan>
                       </text>
                     );
@@ -117,21 +120,58 @@ export function PortfolioAllocationChart() {
         </ChartContainer>
       </CardContent>
       <CardFooter className='flex-col gap-2 text-sm'>
-        {formattedData.map((i) => {
-          return (
-            <p>
-              {i.investment}:{' '}
-              {((i.amountInvested / totalVisitors) * 100).toFixed(2)}%
-            </p>
-          );
-        })}
-        <div className='flex items-center gap-2 leading-none font-medium'>
-          Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
-        </div>
-        <div className='text-muted-foreground leading-none'>
-          Showing total visitors for the last 6 months
+        <ChartFooterCategoryData
+          formattedData={formattedData}
+          totalInvested={totalInvested}
+        />
+        <div className='text-muted-foreground leading-none hover:cursor-default'>
+          Mostrando total de investimentos
         </div>
       </CardFooter>
     </Card>
+  );
+}
+
+type ChartFooterCategoryDataProps = {
+  formattedData: any;
+  totalInvested: number;
+};
+function ChartFooterCategoryData(props: ChartFooterCategoryDataProps) {
+  const { formattedData, totalInvested } = props;
+  const { objectives } = useUserInformation();
+
+  return (
+    <section className='flex flex-col items-start w-full gap-2'>
+      {formattedData.map((i: any) => {
+        const percentInvested = (i.amountInvested / totalInvested) * 100;
+        const isOverObjective =
+          objectives && objectives[i.investment] > percentInvested;
+        return (
+          <span className='flex gap-1 items-center hover:scale-105 transition-all'>
+            <p>
+              {chartConfig?.[i.investment as keyof typeof chartConfig].label}:
+            </p>
+            <p
+              className={`font-bold ${isOverObjective ? 'text-green-400' : 'text-red-400'}`}
+            >
+              {percentInvested.toFixed(2)}%
+            </p>
+            <p>
+              {isOverObjective ? (
+                <LucideArrowBigUp
+                  stroke='oklch(79.2% 0.209 151.711)'
+                  fill='oklch(79.2% 0.209 151.711)'
+                />
+              ) : (
+                <LucideArrowBigDown
+                  stroke='oklch(70.4% 0.191 22.216)'
+                  fill='oklch(70.4% 0.191 22.216)'
+                />
+              )}
+            </p>
+          </span>
+        );
+      })}
+    </section>
   );
 }
